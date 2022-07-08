7a2dc023e7ae54b970c22570372d352c0c56fa05
maaxg <maxgps5@hotmail.com>
2022-07-08 11:29:57 -0300

    rolldice: button and dice class

---
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val roll = Dice(20).roll()
+        val textView: TextView = findViewById(R.id.textView)
+        textView.text = roll.toString()
+
+    }
+}
