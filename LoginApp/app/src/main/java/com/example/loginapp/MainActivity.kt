8b291a4f465a465333d92a7105f98b7a62e7b81c
maaxg <maxgps5@hotmail.com>
2022-07-09 12:00:57 -0300

    loginapp: background image added

---
+++ LoginApp/app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.loginapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_main)
+    }
+}
