e2b995e566bfd58fb6298beb2555e8cb956cca50
maaxg <maxgps5@hotmail.com>
2022-07-13 10:52:04 -0300

    herbavola: bottomsheet was added

--- LoginApp/app/src/main/java/com/example/loginapp/MainActivity.kt
+++ LoginApp/app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.loginapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
+        val modalBottomSheet = ModalBottomSheet()
+        modalBottomSheet.isCancelable = false
+        val behavior = BottomSheetBehavior.from(findViewById(R.id.standard_bottom_sheet))
+        behavior.peekHeight = newHeight
+        modalBottomSheet.show(supportFragmentManager, ModalBottomSheet.TAG)
+
         setContentView(R.layout.activity_main)
     }
 }
