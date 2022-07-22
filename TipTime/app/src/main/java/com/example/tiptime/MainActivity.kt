03725d2d6039d7883c623d67b9205f54693a599e
maaxg <maxgps5@hotmail.com>
2022-07-22 10:34:55 -0300

    feat(tiptime): tip is now beeing calculated

--- TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateTip.setOnClickListener{calculateTip()}
     }
+
+    private fun calculateTip() {
+        val cost = binding.costOfService.text.toString().toDoubleOrNull()
+        if(cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
+        val percentage = getPercentage(binding.tipOptions.checkedRadioButtonId)
+        var tip = cost * percentage
+        tip = if(binding.roundUpSwitch.isChecked) kotlin.math.ceil(tip) else tip
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun getPercentage(checkedRadioButtonId: Int): Double {
+        return when (checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.2
+            R.id.option_eigthteen_percent -> 0.18
+            else -> 0.15
+        }
+    }
+
+
 }
